cfa9b600587c10f36a0e2deb299e84fde525281e
Joshua <joshuamayowa23@yahoo.com>
2022-08-06 23:49:07 +0100

    media updated

--- src/component/SingIn/SignIn.jsx
+++ src/component/SingIn/SignIn.jsx
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import './SignIn.css';
 
 class SignIn extends Component {
-    state = {  } 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLearn: false
+        }
+    }
+
+    learnMore(e) {
+        this.setState({
+            isLearn : !this.state.isLearn
+        })
+        var learn = e.target
+        // console.log(learn)
+
+        if (e.target.firstChild.textContent == "  Learn more.") {
+            console.log(e.target)
+
+            learn.outerHTML = "<br /><br /><div>The information collected by Google reCAPTCHA is subject to the Google <a target='_blank' href='https://policies.google.com/privacy'> Privacy Policy</a> and <a href='https://policies.google.com/terms' target='_blank'>Terms of Service</a>, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google).</div>"
+
+        }
+
+    }
+
     render() { 
         return (
             <div className="login">
@@ -33,7 +55,7 @@ class SignIn extends Component {
                                     <div className="login-form__help">
                                         <div className="remember-me">
                                             <input type="checkbox" name='rememberMe' />
-                                            <label className='' htmlFor="rememberMe"><span>Remember me?</span></label>
+                                            <label className='' htmlFor="rememberMe"><span>Remember me</span></label>
                                         </div>
                                         <a href="" className="login-help">Need help?</a>
                                     </div>
@@ -47,7 +69,7 @@ class SignIn extends Component {
                                 <div className="terms-of-use">
                                     <p>
                                         <span>This page is protected by Google reCAPTCHA to ensure you're not a bot. </span>
-                                        <button className="term-button">  Learn more.</button>
+                                        <button className="term-button" onClick={(e)=>this.learnMore(e)}>  Learn more.</button>
                                     </p>
                                     <div className="nothing"></div>
                                 </div>
@@ -61,7 +83,6 @@ class SignIn extends Component {
                             <ul className="footer-links">
                                 <li className="footer-link__item"><a href="">FAQ</a></li>
                                 <li className="footer-link__item"><a href="">Help Center</a></li>
-                                <li className="footer-link__item"><a href="">Account</a></li>
                                 <li className="footer-link__item"><a href="">Terms of Use</a></li>
                                 <li className="footer-link__item"><a href="">Privacy</a></li>
                                 <li className="footer-link__item"><a href="">Cookie Preferences</a></li>
